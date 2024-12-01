75fda1d442bbd395342db3ef7653f600ae58e68f
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-12-01 18:28:01 -0500

    feat(day1): solve second part

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // read input
@@ -16,12 +16,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         first.push(a);
         second.push(b);
     }
-    let result = total_diff(&mut first, &mut second);
+
+    // print result for part 1
+    let result = get_total_diff(&mut first, &mut second);
     println!("The result of the first part is: {:?}", result);
+
+    // print result for part 2
+    let result = get_similarity(&mut first, &mut second);
+    println!("The result of the second part is: {:?}", result);
     Ok(())
 }
 
-fn total_diff(a: &mut [i64], b: &mut [i64]) -> i64 {
+fn get_total_diff(a: &mut [i64], b: &mut [i64]) -> i64 {
     // sort vectors
     a.sort();
     b.sort();
@@ -38,15 +44,42 @@ fn total_diff(a: &mut [i64], b: &mut [i64]) -> i64 {
     result
 }
 
+fn get_similarity(a: &mut [i64], b: &mut [i64]) -> i64 {
+    // keep track of how often each value appears in vector b
+    let mut counter = HashMap::new();
+    for key in b.iter() {
+        match counter.get(&key) {
+            Some(value) => counter.insert(key, value + 1),
+            None => counter.insert(key, 1),
+        };
+    }
+
+    // compute "similarity" for each element of vector a
+    let mut acc: i64 = 0;
+    for key in a.iter() {
+        let multiplier = counter.get(key).unwrap_or(&0_i64);
+        acc += key * multiplier;
+    }
+    acc
+}
+
 #[cfg(test)]
 mod test {
-    use crate::total_diff;
+    use crate::{get_similarity, get_total_diff};
 
     #[test]
-    fn test_simple() {
+    fn test_part1() {
         let mut a = vec![10, 1, 2];
         let mut b = vec![10, 2, 0];
-        let result = total_diff(&mut a, &mut b);
+        let result = get_total_diff(&mut a, &mut b);
         assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_part2() {
+        let mut a = vec![10, 1, 2];
+        let mut b = vec![10, 2, 10];
+        let result = get_similarity(&mut a, &mut b);
+        assert_eq!(result, 22);
     }
 }
