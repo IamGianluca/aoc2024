3a1211d94040f3fbb96d906acc639f3e0e008f39
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-12-07 19:33:27 -0500

    refactor(day3): use state machine for part 2; still cannot pass online check

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -5,25 +5,36 @@ use regex::Regex;
 fn main() {
     let input = fs::read_to_string("./day3_data.txt").unwrap();
 
-    let mut result = 0;
+    let mut sum_p1 = 0;
     for instruction in input.lines() {
-        result += compute(instruction);
+        sum_p1 += compute(instruction);
     }
-    println!("Part 1 result: {:?}", result);
+    println!("Part 1 result: {:?}", sum_p1);
 
-    let mut result = 0;
+    let mut sum_p2 = 0;
     for instruction in input.lines() {
-        result += compute_does_and_donts(instruction);
+        sum_p2 += compute_dos_and_donts(instruction);
     }
-    println!("Part 2 result: {:?}", result);
+    println!("Part 2 result: {:?}", sum_p2);
 }
 
-fn compute_does_and_donts(instruction: &str) -> u64 {
-    println!("{:?}", instruction);
-    let re = Regex::new(r"don't\(\).*?(?:do\(\)|$)").unwrap();
-    let new_instruction = re.replace_all(instruction, "").to_string();
-    println!("{:?}", new_instruction);
-    compute(&new_instruction)
+fn compute_dos_and_donts(instruction: &str) -> u64 {
+    let re = Regex::new(r"(mul\((\d+),(\d+)\))|do\(\)|don't\(\)").unwrap();
+    let mut result = 0;
+    let mut mul_enabled = true;
+    for cap in re.captures_iter(instruction) {
+        if let Some(v) = cap.get(1) {
+            if mul_enabled {
+                let mul_str = v.as_str();
+                result += compute(mul_str);
+            }
+        } else if cap.get(0).map_or("", |m| m.as_str()) == "do()" {
+            mul_enabled = true;
+        } else if cap.get(0).map_or("", |m| m.as_str()) == "don't()" {
+            mul_enabled = false;
+        }
+    }
+    result
 }
 
 fn compute(instruction: &str) -> u64 {
@@ -37,7 +48,7 @@ fn compute(instruction: &str) -> u64 {
 
 #[cfg(test)]
 mod test {
-    use crate::{compute, compute_does_and_donts};
+    use crate::{compute, compute_dos_and_donts};
 
     #[test]
     fn test_something() {
@@ -49,68 +60,68 @@ mod test {
     fn test_does_and_donts() {
         let instruction =
             "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-        assert_eq!(compute_does_and_donts(instruction), 48);
+        assert_eq!(compute_dos_and_donts(instruction), 48);
     }
 
     #[test]
     fn test_does_and_donts_until_end_of_string() {
         let instruction = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)()?mul(8,5))";
-        assert_eq!(compute_does_and_donts(instruction), 8);
+        assert_eq!(compute_dos_and_donts(instruction), 8);
     }
 
     #[test]
     fn test_does_and_donts_not_reenabled() {
         let instruction = "xmul(2,4)&mul[3,7]!^dn't()_mul(5,5)+mul(32,64](mul(11,8)und()?mul(8,5))";
-        assert_eq!(compute_does_and_donts(instruction), 161);
+        assert_eq!(compute_dos_and_donts(instruction), 161);
     }
 
     #[test]
     fn test_does_and_donts_nothing() {
         let instruction =
             "don't()xmul(2,4)&mul[3,7]!^dn't()_mul(5,5)+mul(32,64](mul(11,8)und()?mul(8,5))";
-        assert_eq!(compute_does_and_donts(instruction), 0);
+        assert_eq!(compute_dos_and_donts(instruction), 0);
     }
 
     #[test]
     fn test_does_and_donts_overlapping() {
         let instruction = "don't() mul(1,1) do() mul(2,2) don't() mul(3,3) do() mul(4,4)";
-        assert_eq!(compute_does_and_donts(instruction), 20);
+        assert_eq!(compute_dos_and_donts(instruction), 20);
     }
 
     #[test]
     fn test_nested_donts() {
         let instruction = "don't() mul(1,1) don't() mul(2,2) do() mul(3,3)";
-        assert_eq!(compute_does_and_donts(instruction), 9);
+        assert_eq!(compute_dos_and_donts(instruction), 9);
     }
     #[test]
     fn test_order_counts() {
         let instruction = "don't() mul(1,1) don't() mul(2,2) do() mul(3,3)";
-        assert_eq!(compute_does_and_donts(instruction), 9);
+        assert_eq!(compute_dos_and_donts(instruction), 9);
     }
 
     #[test]
     fn test_multiple() {
         let instruction = "mul(1,1) don't() do() don't() mul(2,2)";
-        assert_eq!(compute_does_and_donts(instruction), 1);
+        assert_eq!(compute_dos_and_donts(instruction), 1);
     }
 
     #[test]
     fn test_multiple_even_more() {
         let instruction = "mul(1,1) don't() do() don't() do() don't() mul(2,2)";
-        assert_eq!(compute_does_and_donts(instruction), 1);
+        assert_eq!(compute_dos_and_donts(instruction), 1);
     }
 
     #[test]
     fn test_ug() {
         let instruction =
             "mul(1,1) don't() mul(2,2) don't() mul(3,3) do() don't() do() don't() mul(2,2)";
-        assert_eq!(compute_does_and_donts(instruction), 1);
+        assert_eq!(compute_dos_and_donts(instruction), 1);
     }
 
     #[test]
     fn test_most_complex_case() {
         let instruction =
             "mul(1,1) do() mul(2,2) don't() mul(3,3) do() mul(4,4) don't() don't() mul(5,5) do() mul(6,6) don't() mul(7,7) do() mul(8,8) don't() mul(9,9)";
-        assert_eq!(compute_does_and_donts(instruction), 121);
+        assert_eq!(compute_dos_and_donts(instruction), 121);
     }
 }
