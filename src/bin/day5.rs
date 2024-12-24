d4f97f5e8c5a1106fc41348a379e1086f809fae5
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-12-23 19:29:07 -0500

    feat(day5): solve part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -2,15 +2,17 @@ use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("./day5_data.txt").unwrap();
-    let result = solve_puzzle(input.as_str());
+    let result = solve_puzzle(input.as_str(), 1);
     println!("Solution part 1: {:?}", result);
-    println!("Solution part 2: {:?}", 0);
+
+    let result = solve_puzzle(input.as_str(), 2);
+    println!("Solution part 2: {:?}", result);
 }
 
-fn solve_puzzle(input: &str) -> u64 {
+fn solve_puzzle(input: &str, part: u64) -> u64 {
     let sections: Vec<&str> = input.split("\n\n").collect();
     let rules = process_rules(sections[0]);
-    process_lists(sections[1], rules)
+    process_lists(sections[1], rules, part)
 }
 
 fn process_rules(input: &str) -> HashMap<u64, Vec<u64>> {
@@ -39,14 +41,21 @@ fn process_rule(m: &mut HashMap<u64, Vec<u64>>, input: &str) {
     };
 }
 
-fn process_lists(input: &str, rules: HashMap<u64, Vec<u64>>) -> u64 {
+fn process_lists(input: &str, rules: HashMap<u64, Vec<u64>>, part: u64) -> u64 {
     let mut result = 0;
+    let func = if part == 1 {
+        process_list_part1 as fn(Vec<u64>, &HashMap<u64, Vec<u64>>) -> u64
+    } else if part == 2 {
+        process_list_part2 as fn(Vec<u64>, &HashMap<u64, Vec<u64>>) -> u64
+    } else {
+        panic!("Part {:?}", part);
+    };
     for line in input.lines() {
         let line = line
             .split(",")
             .map(|x| x.trim().parse::<u64>().unwrap())
             .collect();
-        result += process_list_part1(line, &rules);
+        result += func(line, &rules);
     }
     result
 }
@@ -56,6 +65,15 @@ fn process_list_part1(line: Vec<u64>, rules: &HashMap<u64, Vec<u64>>) -> u64 {
         line[line.len() / 2]
     } else {
         0
+    }
+}
+
+fn process_list_part2(line: Vec<u64>, rules: &HashMap<u64, Vec<u64>>) -> u64 {
+    if is_correct_order(&line, rules) {
+        0
+    } else {
+        let new_line = sort_sequence(&line, rules);
+        new_line[new_line.len() / 2]
     }
 }
 
@@ -80,36 +98,41 @@ fn is_correct_order(line: &[u64], rules: &HashMap<u64, Vec<u64>>) -> bool {
     true
 }
 
-fn sort_sequence(sequence: &str, map: &HashMap<u64, Vec<u64>>) -> Vec<u64> {
-    let mut sequence: Vec<u64> = sequence
-        .split(",")
-        .map(|x| x.parse::<u64>().unwrap())
-        .collect();
-
-    for i in 1..sequence.len() {
-        let curr = sequence[i];
-        let prev = sequence[i - 1];
+fn sort_sequence(sequence: &Vec<u64>, map: &HashMap<u64, Vec<u64>>) -> Vec<u64> {
+    let mut result = sequence.clone();
+    for i in 1..result.len() {
+        let curr = result[i];
+        let prev = result[i - 1];
         match map.get(&curr) {
             Some(v) => {
                 if v.contains(&prev) {
-                    println!("Found");
                     continue;
                 } else {
-                    println!("Not Found");
-                    let element = sequence.remove(i);
+                    let element = result.remove(i);
                     for e in (0..i).rev() {
-                        println!("{:?}", e);
-                        if v.contains(&sequence[e]) {
-                            sequence.insert(e + 1, element);
-                            continue;
+                        if v.contains(&result[e]) {
+                            result.insert(e + 1, element);
+                            break;
                         }
                     }
+                    if result.len() != sequence.len() {
+                        // if loop not escaped already, it means that the elem should be moved at the
+                        // beginning of the sequence
+                        result.insert(0, element);
+                    };
                 }
             }
             None => return vec![9999],
         }
     }
-    sequence
+    if result.len() != sequence.len() {
+        panic!(
+            "Result and Sequence do not have the same length ({} vs. {})",
+            result.len(),
+            sequence.len()
+        );
+    };
+    result
 }
 
 #[cfg(test)]
@@ -140,7 +163,7 @@ mod tests {
         97|13
 
         97,13,75";
-        assert_eq!(solve_puzzle(input), 13);
+        assert_eq!(solve_puzzle(input, 1), 13);
     }
 
     #[test]
@@ -149,7 +172,7 @@ mod tests {
         97|13
 
         97,75,13";
-        assert_eq!(solve_puzzle(input), 0);
+        assert_eq!(solve_puzzle(input, 1), 0);
     }
 
     #[test]
@@ -182,7 +205,7 @@ mod tests {
         75,97,47,61,53
         61,13,29
         97,13,75,29,47";
-        assert_eq!(solve_puzzle(input), 143);
+        assert_eq!(solve_puzzle(input, 1), 143);
     }
 
     #[test]
@@ -192,10 +215,10 @@ mod tests {
         29|13
         61|29 ";
         let rules = process_rules(rules_str);
-        let sequence = "61,13,29";
+        let sequence = vec![61, 13, 29];
 
         // when
-        let result = sort_sequence(sequence, &rules);
+        let result = sort_sequence(&sequence, &rules);
 
         // then
         let expected = vec![61, 29, 13];
