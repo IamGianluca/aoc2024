8333f709004e94113aa1f14eaf66cc908e2e9428
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-12-27 20:27:33 -0500

    feat(day6): solve part 1

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,21 +1,25 @@
+use std::fs;
+
 fn main() {
-    todo!();
+    let input = fs::read_to_string("./day6_data.txt").unwrap();
+    let result_p1 = solve_part1(input.as_str());
+    println!("Result part 1: {:?}", result_p1)
 }
 
 #[allow(dead_code)]
 fn solve_part1(input: &str) -> u64 {
-    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
     const DIRECTIONS: [(i64, i64); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
     let mut direction_index = 0;
-    for line in grid.iter() {
-        println!("{:?}", line);
-    }
+    // for line in grid.iter() {
+    //     println!("{:?}", line);
+    // }
     let mut current_position = get_starting_position(&grid);
-    println!(
-        "Starting position: ({:?}, {:?})",
-        current_position.0, current_position.1
-    );
+    // println!(
+    //     "Starting position: ({:?}, {:?})",
+    //     current_position.0, current_position.1
+    // );
 
     // Move until we reach the end of the board. If there is something directly in front of
     // you, turn right 90 degrees. Otherwise, take a step forward.
@@ -37,34 +41,29 @@ fn solve_part1(input: &str) -> u64 {
         // And don't forget to update continue_game to false if the game is over.
         let candidate = match grid.get(candidate_position.0) {
             Some(v) => v,
-            None => return steps_count,
+            None => return count_visited(&grid),
         };
         let candidate = match candidate.get(candidate_position.1) {
             Some(v) => v,
-            None => return steps_count,
+            None => return count_visited(&grid),
         };
-        println!("Step: {:?}, Value: {:?}", steps_count, candidate);
+        // println!("Step: {:?}, Value: {:?}", steps_count, candidate);
         match candidate {
             '#' => {
                 // Skip and turn right next time
                 direction_index += 1;
             }
-            '.' => {
+            '.' | 'x' | '^' => {
                 // Valid landing spot
                 current_position = candidate_position;
-                steps_count += 1;
-                continue;
-            }
-            '^' => {
-                // Valid landing spot
-                current_position = candidate_position;
+                grid[current_position.0][current_position.1] = 'x';
                 steps_count += 1;
                 continue;
             }
             _ => continue_game = false,
         };
     }
-    steps_count
+    0
 }
 
 fn get_starting_position(grid: &Vec<Vec<char>>) -> (usize, usize) {
@@ -79,9 +78,23 @@ fn get_starting_position(grid: &Vec<Vec<char>>) -> (usize, usize) {
     (99999, 99999)
 }
 
+fn count_visited(grid: &Vec<Vec<char>>) -> u64 {
+    let mut result = 0;
+    for r in grid.iter() {
+        for c in r.iter() {
+            if *c == 'x' || *c == '^' {
+                result += 1;
+            }
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{get_starting_position, solve_part1};
+    use std::fs;
+
+    use crate::{count_visited, get_starting_position, solve_part1};
 
     #[test]
     fn test_find_starting_point() {
@@ -105,8 +118,10 @@ mod test {
         let expected = (6, 4);
         assert_eq!(result, expected);
     }
+
     #[test]
     fn test_simple_case_part1() {
+        // Given
         let input = "....#.....
 .........#
 ..........
@@ -117,7 +132,28 @@ mod test {
 ........#.
 #.........
 ......#...";
+
+        // When
         let result = solve_part1(input);
+
+        // Then
         assert_eq!(result, 41);
+    }
+
+    #[test]
+    fn test_count_visited() {
+        let grid = "....x.....
+....x....#
+....x.....
+..#.x.....
+....x..#..
+....x.....
+.#..^.....
+........#.
+#.........
+......#...";
+        let grid = grid.lines().map(|line| line.chars().collect()).collect();
+        let result = count_visited(&grid);
+        assert_eq!(result, 7)
     }
 }
