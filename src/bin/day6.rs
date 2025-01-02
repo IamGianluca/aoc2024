97fec671d88bd5d688aafd479580bde9c7ab2fbd
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2025-01-01 20:54:34 -0500

    feat(day6): handle cases where the infinite loop can be caused by bouncing on obstacle unseen in travelled route

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -80,16 +80,36 @@ fn count_visited(grid: &Vec<Vec<char>>) -> u64 {
 }
 
 fn solve_part2(input: &str) -> u64 {
-    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    const DIRECTIONS: [(i64, i64); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)]; // up, right, down,
-                                                                            // left
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let start_pos = get_starting_position(&grid);
+    let mut result = 0;
+    for r in 0..grid.len() {
+        for c in 0..grid[0].len() {
+            if grid[r][c] == '#' || (r, c) == start_pos {
+                continue;
+            }
+            let mut tmp_grid = grid.clone();
+            tmp_grid[r][c] = '#';
+            if creates_loop(&mut tmp_grid, start_pos) {
+                result += 1;
+            }
+        }
+    }
+    result
+}
+
+fn creates_loop(grid: &mut Vec<Vec<char>>, start_pos: (usize, usize)) -> bool {
+    const DIRECTIONS: [(i64, i64); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mut visited_pos = vec![];
     let mut direction_index = 0;
-    let mut current_position = get_starting_position(&grid);
+    let mut current_position = start_pos;
+
+    // TODO: passing by the same position multiple times is not necessarily a sign we are in an
+    // infinite loop. Maybe is making the same turn?
 
     // Move until we reach the end of the board. If there is something directly in front of
     // you, turn right 90 degrees. Otherwise, take a step forward.
     let mut continue_game = true;
-    let mut obstacle_positions: Vec<(usize, usize)> = vec![];
     while continue_game {
         // Propose next step
         let direction = DIRECTIONS[direction_index % 4];
@@ -102,75 +122,40 @@ fn solve_part2(input: &str) -> u64 {
         // the game is over.
         let candidate = match grid.get(candidate_position.0) {
             Some(v) => v,
-            None => return count_loops(&grid),
+            None => return false,
         };
         let candidate = match candidate.get(candidate_position.1) {
             Some(v) => v,
-            None => return count_loops(&grid),
+            None => return false,
         };
         match candidate {
             '#' => {
                 // Skip and turn right next time
                 direction_index += 1;
-                // Save position of every obstacle we encounter
-                obstacle_positions.push(candidate_position);
             }
-            '.' | 'x' | '^' | 'o' => {
-                let (op1, op2): (
-                    Box<dyn Fn(usize, usize) -> bool>,
-                    Box<dyn Fn(usize, usize) -> bool>,
-                ) = match direction {
-                    (-1, 0) => (
-                        Box::new(|a: usize, b: usize| a == b),
-                        Box::new(|a: usize, b: usize| a < b),
-                    ), // up
-                    (0, 1) => (
-                        Box::new(|a: usize, b: usize| a < b),
-                        Box::new(|a: usize, b: usize| a == b),
-                    ), // right
-                    (1, 0) => (
-                        Box::new(|a: usize, b: usize| a == b),
-                        Box::new(|a: usize, b: usize| a > b),
-                    ), // down
-                    (0, -1) => (
-                        Box::new(|a: usize, b: usize| a > b),
-                        Box::new(|a: usize, b: usize| a == b),
-                    ), // left
-                    _ => panic!("Invalid direction"),
-                };
-                let interesting_obstacles: Vec<(usize, usize)> = obstacle_positions
-                    .iter()
-                    .filter(|(x, y)| op1(current_position.0, *x) && op2(current_position.1, *y))
-                    .cloned()
-                    .collect();
-                if !interesting_obstacles.is_empty() {
-                    grid[candidate_position.0][candidate_position.1] = 'o';
-                } else if candidate != &'o' {
-                    grid[candidate_position.0][candidate_position.1] = 'x';
-                }
-                // Move to candidate position
+            '.' | 'x' | '^' => {
+                // Valid landing position
                 current_position = candidate_position;
-                continue;
+                let count = visited_pos
+                    .iter()
+                    .filter(|&&x| x == current_position)
+                    .count();
+                if count >= 3 {
+                    println!("Found another match");
+                    for line in grid.iter() {
+                        println!("{:?}", line);
+                    }
+                    return true;
+                } else {
+                    visited_pos.push(current_position);
+                    grid[current_position.0][current_position.1] = 'x';
+                    continue;
+                };
             }
             _ => continue_game = false,
         };
     }
-    0
-}
-
-fn count_loops(grid: &Vec<Vec<char>>) -> u64 {
-    // for (i, line) in grid.iter().enumerate() {
-    //     println!("[{:?}] {:?}", i, line);
-    // }
-    let mut result = 0;
-    for r in grid.iter() {
-        for c in r.iter() {
-            if *c == 'o' {
-                result += 1;
-            }
-        }
-    }
-    result
+    false
 }
 
 #[cfg(test)]
@@ -262,5 +247,37 @@ mod test {
 
         // Then
         assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn test_add_one_obstacle_make_infinite_loop() {
+        // Given
+        let input = ".#..
+...#
+.^..
+..#.";
+
+        // When
+        let result = solve_part2(input);
+
+        // Then
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_bouncing_on_obstacle_not_previously_visited_creates_infinite_loop() {
+        // Given
+        let input = "..#..
+....#
+.#...
+..^..
+#....
+...#.";
+
+        // When
+        let result = solve_part2(input);
+
+        // Then
+        assert_eq!(result, 3);
     }
 }
